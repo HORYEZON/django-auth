d9816309c03bbffc18cfdec6a3818e3a74dc011a
HORYEZON <jryan.briz@gmail.com>
2025-05-24 19:47:59 +0800

    added Logout

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    is_verified = models.BooleanField(default=False) 
 
     def __str__(self):
         return self.user.username
